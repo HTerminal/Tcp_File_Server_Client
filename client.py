bb6483140df831c397a2a444820cb12d4e6d9840
Hit_terminal <hiteshblog1@gmail.com>
2017-07-15 11:49:58 +0530

    Transfer file using TCP
    You can transfer files using server and client. Made it to learn TCP communication

---
+++ client.py
@@ -0,0 +1,46 @@
+import socket # Import socket module
+import os
+import time
+from functools import wraps
+p_start_time = time.time()
+print("--- %s seconds ---" % (time.time() - p_start_time))
+
+
+print '########################You are Client streaming#################'
+ip=raw_input('Enter server ip address:')
+port = input('Enter server port no.:')
+file = raw_input('Enter file to stream with extenstion.:')
+
+file_size=os.path.getsize(file)
+file_size = file_size/1024
+print 'File size',file_size,'KB'
+
+print 'File size',file_size/1024,'MB'
+s = socket.socket()         # Create a socket object
+host = ip # Get local machine name
+port = port              # Reserve a port for your service.
+
+s.connect((host, port))
+
+s.send(str(file_size))
+f = open(file,'rb')
+print s.recv(100240)
+stream=raw_input('Press! Enter to STREAM:')
+start_time = time.time()
+print 'Streaming...',file
+l = f.read(100240)
+while (l):
+    
+    print 'Sending...'
+    s.send(l)
+    l = f.read(100240)
+f.close()
+print "Done Sending"
+s.shutdown(socket.SHUT_WR)
+print s.recv(100240)
+#s.send('ok')
+
+s.close                     # Close the socket when done
+elapsed_time = time.time() - start_time
+#print("--- %s seconds ---" % (time.time() - elapsed_time))
+print elapsed_time
---
+++ server.py
@@ -0,0 +1,64 @@
+import socket               # Import socket module
+import os
+import time
+file_size=0;
+check = 460;
+elapsed_time = 0;
+p_start_time = time.time()
+print("--- %s seconds ---" % (time.time() - p_start_time))
+print '########################You are server receiving#################'
+ip=raw_input('Enter your ip address:')
+port = input('Enter your port no.:')
+file_name = raw_input('Enter the file name you are reciveing with .extention:')
+
+s = socket.socket()         # Create a socket object
+host = ip                   # Get local machine name
+port = port                 # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+f = open(file_name,'w+b')
+s.listen(5)                 # Now wait for client connection.
+print 'Listining----'
+while True:
+    c, addr = s.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    
+    size_file=c.recv(8024)
+    #hold=raw_input('Enter to send server connected msg');
+    c.send('server connected!')
+    
+    
+    print 'Incoming stream size',size_file,'KB'
+    
+    
+    print "Streaming..."
+    start_time = time.time()
+    l = c.recv(8024)
+    while (l):
+        print "Receiving..."
+        
+        f.write(l)
+        if file_size == 460:  #if you want to get exit then enable it
+            print 'Closing'
+            s.close()
+            c.close()
+            
+    
+        l = c.recv(8024)
+        #if int(elapsed_time) == 5:
+        #    os.system("on.bat");
+        file_size=os.path.getsize('recive.mp4')
+        file_size = file_size/1024
+        
+        print file_size,'KB','/',size_file,'KB'
+        elapsed_time = time.time() - start_time
+        print elapsed_time
+        
+        
+    f.close()
+    
+c.close()
+    
+        
+    
+
+
